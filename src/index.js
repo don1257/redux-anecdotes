f2af381a64de7351f766ae44db660c0df961f5bc
Xiao Huoguo <jp-hok>
2023-06-01 13:02:42 -0400

    Added store

--- src/index.js
+++ src/index.js
@@ -2,20 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
-import filterReducer from "./reducers/filterReducer";
-import anecdoteReducer from "./reducers/anecdoteReducer";
-import {configureStore} from "@reduxjs/toolkit";
-import notificationReducer from "./reducers/notificationReducer";
+import store from "./store";
 
-const store = configureStore({
-    reducer: {
-        anecdote: anecdoteReducer,
-        filter: filterReducer,
-        notification: notificationReducer,
-    }
-})
-
-console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
